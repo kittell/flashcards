3287a0ffe296d9c7fb18f88906d944bf9fd2a531
Kirk Kittell <kirk.kittell@gmail.com>
2020-03-24 16:58:59 -0500

    small tk changes

--- src/flashcards_gui.py
+++ src/flashcards_gui.py
@@ -23,13 +23,19 @@ class FlashcardsGUI(tk.Frame):
         self.start_screen()
         
     def start_screen(self):
-        self.start = tk.Button(self)
-        self.start["text"] = "Start"
-        self.start["command"] = self.start_round
-        self.start.pack(side="left")
+        self.start_button = tk.Button(self)
+        self.start_button["text"] = "Start"
+        self.start_button["command"] = self.start_round
+        self.start_button.pack(side="left")
         
-        self.quit = tk.Button(self, text="Quit", command=self.master.destroy)
-        self.quit.pack(side="left")
+        self.quit_button = tk.Button(self, text="Quit", command=self.master.destroy)
+        self.quit_button.pack(side="left")
         
     def start_round(self):
         print("Start round")
+        
+        # TODO load deck - meanwhile, hardcode a loaded deck
+        self.select_button = tk.Button(self)
+        self.select_button["text"] = "Select deck"
+        self.select_button.pack(side="right")
+        
