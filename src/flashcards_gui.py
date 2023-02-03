832c0fea7dd7e829a36ef133ac0c32776942ea68
Kirk Kittell <kirk.kittell@gmail.com>
2023-02-03 13:18:37 -0600

    Update flashcards_gui.py

--- src/flashcards_gui.py
+++ src/flashcards_gui.py
@@ -1,41 +1,24 @@
-'''
-Created on Mar 23, 2020
-
-@author: sd931e
-'''
-
 import tkinter as tk
+from tkinter import ttk
 
-class FlashcardsApp:
-    def __init__(self):
-        root = tk.Tk()
-        app = FlashcardsGUI(master=root)
-        app.mainloop()
+class FlashcardsGUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.frame_main = ttk.Frame(self, padding=5)
+        
+        def callback_start_round():
+            print("Start round")
+            
+            # TODO load deck - meanwhile, hardcode a loaded deck
+            self.select_button = tk.Button(self)
+            self.select_button["text"] = "Select deck"
+            self.select_button.pack(side="right")
     
-    def start_screen(self):
-        pass
+        self.title('Flashcards')
+        self.frame_main.grid()
+        
+        
+        self.quit_button = tk.Button(self.frame_main, text='Quit', command=self.destroy)
+        self.quit_button.grid()
 
-class FlashcardsGUI(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.start_screen()
-        
-    def start_screen(self):
-        self.start_button = tk.Button(self)
-        self.start_button["text"] = "Start"
-        self.start_button["command"] = self.start_round
-        self.start_button.pack(side="left")
-        
-        self.quit_button = tk.Button(self, text="Quit", command=self.master.destroy)
-        self.quit_button.pack(side="left")
-        
-    def start_round(self):
-        print("Start round")
-        
-        # TODO load deck - meanwhile, hardcode a loaded deck
-        self.select_button = tk.Button(self)
-        self.select_button["text"] = "Select deck"
-        self.select_button.pack(side="right")
-        
+
